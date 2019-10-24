4538143dc0a70b64d7ea1908eb4d7a98cb092bd3
Volodymyr Lytvynchyk <volodymyr.lytvynchyk@outlook.com>
2019-10-24 14:44:14 +0300

    Integrated obfuscator.

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
 cd src
 msbuild /restore .\ErrorWhileMerging.sln -p:Configuration=Release
-Eazfuscator.NET.exe "MainProject\bin\Release\netstandard2.0\MainProject.dll" --msbuild-project-path "MainProject\MainProject.csproj" --msbuild-project-configuration "Release" --output "."
+#Eazfuscator.NET.exe "MainProject\bin\Release\netstandard2.0\MainProject.dll" --msbuild-project-path "MainProject\MainProject.csproj" --msbuild-project-configuration "Release" --output "."
+cd ..
